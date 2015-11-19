81e0e5fa25f235853a6ef4614b14305e0986eb59
Andreas Klein <mail@andreasklein.org>
2015-11-19 11:49:59 +0100

    Fix testimonial slider mobile

--- _modules/_slider/slider.js
+++ _modules/_slider/slider.js
@@ -1,24 +1,11 @@
 var $ = require('jquery');
 var Slick = require('../../node_modules/slick-carousel/slick/slick');
-var FastClick = require('fastclick');
-
 
 $('[data-slider]').each(function () {
-
-    var $this = $(this);
-    var mobile = $this.data('slider-mobile') === false ? {
-                    breakpoint: 480,
-                    settings: 'unslick'
-                } : {
-                    breakpoint: 480,
-                    settings: {
-                        arrows: false
-                    }
-                };
-
-    $this.slick({
+    $(this).slick({
         draggable: false,
         accessibility: false,
+        adaptiveHeight: true,
         prevArrow:
             '<a class="slick-prev">' +
                 '<svg viewBox="0 0 24 24" class="icon icon--large">' +
@@ -29,24 +16,6 @@ $('[data-slider]').each(function () {
                         '<svg viewBox="0 0 24 24" class="icon icon--large">' +
                             '<use xlink:href="assets/svg/sprite.symbol.svg#arrow-down"></use>' +
                         '</svg>' +
-                    '</a>',
-        responsive: [mobile]
-    });
-});
-
-$('[data-slider-mobile-more]').each(function () {
-    var $this = $(this);
-
-    // Get corresponding Slider Instance
-    var sliderId = $this.data('slider-mobile-more');
-    var $slider = $('[data-slider="' + sliderId +'"');
-
-    // Add EventListerner and enable FastClick on Nav Button
-    // FastClick($this[0]);
-
-    $this.on('click', function () {
-        alert('test');
-        $slider.addClass('is--opened');
-        $this.remove();
+                    '</a>'
     });
 });
